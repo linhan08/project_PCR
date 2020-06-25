a4cc742e674d82c40e561d06ed5441ab58293964
daofficial88@gmail.com <linhtram0601@gmail.com>
2020-06-25 20:39:37 +0700

    new

---
+++ PCR_sys/src/main/java/com/ifisolution/pcrangular/PcrSysApplication.java
@@ -0,0 +1,13 @@
+package com.ifisolution.pcrangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcrSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcrSysApplication.class, args);
+	}
+
+}
---
+++ PCR_sys/src/test/java/com/ifisolution/pcrangular/PcrSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifisolution.pcrangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcrSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
