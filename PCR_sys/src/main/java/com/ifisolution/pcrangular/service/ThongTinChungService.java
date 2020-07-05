7990cdcb5cb52a946fe1603d00991079d464f986
daofficial88@gmail.com <linhtram0601@gmail.com>
2020-07-05 21:25:42 +0700

    new

--- PCR_sys/src/main/java/com/ifisolution/pcrangular/service/ThongTinChungService.java
+++
@@ -1,7 +0,0 @@
-package com.ifisolution.pcrangular.service;
-
-import com.ifisolution.pcrangular.entities.ThongTinChung;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ThongTinChungService extends JpaRepository<ThongTinChung, String> {
-}
