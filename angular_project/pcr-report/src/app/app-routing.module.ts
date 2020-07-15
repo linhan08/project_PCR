4c26646b48b82f9095773d0783900db75b4e1ba4
An Van Linh <daofficial88@gmail.com>
2020-07-15 17:02:59 +0700

    addd

--- angular_project/pcr-report/src/app/app-routing.module.ts
+++ angular_project/pcr-report/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent} from './login/login.component';
-import {DashboardComponent} from './dashboard/dashboard.component'
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {StudentListComponent} from './student-list/student-list.component';
+import {AddStudentComponent} from './add-student/add-student.component';
 
 
 const routes: Routes = [
   {path: '', component: DashboardComponent},
+  {path: '', component: AddStudentComponent},
+  {path: '', component: StudentListComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'view-student', component: StudentListComponent},
+  {path: 'add-student', component: AddStudentComponent},
   {path: 'dashboard', component: DashboardComponent}
 ];
 
