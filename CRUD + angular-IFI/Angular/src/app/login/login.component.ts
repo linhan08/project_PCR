a4b2694fc821dae48aba7161e7ce4d88f4984a4b
daofficial88@gmail.com <linhtram0601@gmail.com>
2020-07-18 10:28:28 +0700

    aaaa

--- CRUD + angular-IFI/Angular/src/app/login/login.component.ts
+++ CRUD + angular-IFI/Angular/src/app/login/login.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Login } from '../model/login';
-import { Register } from '../model/register';
-import { AuthService } from '../services/auth.service';
-import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -10,30 +6,10 @@ import { TokenStorageService } from '../services/token-storage.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginModel: Login = new Login();
-  signModel: Register = new Register();
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+
+  constructor() { }
 
   ngOnInit() {
   }
- 
-  clickSignUp() {
-    document.querySelector('.cont').classList.toggle('s-signup');
-  } 
 
-  login() {
-    this.authService.login(this.loginModel).subscribe(data => {
-      console.log(data);
-      this.tokenStorage.saveToken(data.token);
-    }, err => {
-
-    });
-  }
-  signupUser() {
-    this.authService.register(this.signModel).subscribe(data => {
-      console.log(data);
-    }, err => {
-      console.log(err);
-    });
-  }
 }
